627f3b71997bc2276a11cc65e7afc388fa4b847f
Israel S. Santos <israel.santos@zukk.com.br>
2022-07-07 13:43:45 -0300

    new: form new game labels and input added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { CarouselModule } from 'primeng/carousel';
 import { GalleriaModule } from 'primeng/galleria';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
+import { ChipsModule } from 'primeng/chips';
+import { InputMaskModule } from 'primeng/inputmask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +19,9 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { LoginComponent } from './components/login/login.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { NewGameComponent } from './components/new-game/new-game.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [
@@ -36,7 +42,13 @@ import { NewGameComponent } from './components/new-game/new-game.component';
     CarouselModule,
     ButtonModule,
     ToastModule,
-    GalleriaModule
+    GalleriaModule,
+    FormsModule,
+    InputTextModule,
+    MultiSelectModule,
+    InputNumberModule,
+    ChipsModule,
+    InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/new-game/new-game.component.ts
+++ src/app/components/new-game/new-game.component.ts
@@ -7,7 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewGameComponent implements OnInit {
 
-  constructor() { }
+  public plataforms: any;
+  public genres: any;
+
+
+  constructor() {
+    this.plataforms = [
+      { name: 'PS', code: 1, },
+      { name: 'PS2', code: 2 },
+      { name: 'PS3', code: 3 },
+      { name: 'PS4', code: 4 },
+      { name: 'PS5', code: 5 },
+      { name: 'PSP', code: 6 },
+      { name: 'XBOX', code: 7 },
+      { name: 'XBOX 360', code: 8 },
+      { name: 'XBOX ONE', code: 9 },
+      { name: 'XBOX SERIES S', code: 10 },
+      { name: 'XBOX SERIES X', code: 11 },
+      { name: 'SWITCH', code: 12 },
+      { name: 'SUPER NINTENDO', code: 13 },
+      { name: 'NINTENDO', code: 14 },
+      { name: 'NINTENDO 64', code: 15 },
+      { name: 'NINTENDO 3DS', code: 16 },
+      { name: 'NINTENDO DS', code: 17 },
+      { name: 'NINTENDO SWITCH', code: 18 },
+      { name: 'NINTENDO WII', code: 19 },
+      { name: 'MEGA DRIVE', code: 20 },
+      { name: 'PC', code: 21 },
+      { name: 'MOBILE', code: 22 },
+    ]
+
+    this.genres = [
+      { name: 'Fight', code: 1 },
+      { name: 'Sports', code: 2 },
+      { name: 'Survival', code: 3 },
+      { name: 'Horror', code: 4 },
+      { name: 'RPG', code: 5 },
+      { name: 'Fps', code: 6 },
+      { name: 'Tps', code: 7 },
+      { name: 'Platform', code: 8 },
+      { name: 'Adventure', code: 9 },
+      { name: 'Action', code: 10 },
+      { name: 'Minigame', code: 11 },
+      { name: 'Racing', code: 12 },
+      { name: 'Strategy', code: 14 },
+      { name: 'Musical', code: 15 },
+      { name: 'Dance', code: 16 },
+      { name: 'Simulation', code: 17 },
+    ]
+
+  }
 
   ngOnInit(): void {
   }
